3f3a98a5f4171adfa2b8d7e028204a0e900f067c
Sylvain Defresne <sdefresne@gmail.com>
2012-04-29 21:31:59 +0100

    Add some key bindings to SDMainController

--- Haruhi/SDMainController.m
+++ Haruhi/SDMainController.m
@@ -28,4 +28,24 @@
 - (void)advanceTimeBy:(float)deltaTime {
 }
 
+- (void)keyDown:(NSEvent *)theEvent {
+  NSString *keyString = [theEvent charactersIgnoringModifiers];
+  if ([keyString length] == 1) {
+    unichar keyChar = [keyString characterAtIndex:0];
+    switch (keyChar) {
+      case 'q':
+      case 'Q':
+      case 27:
+        [[NSApplication sharedApplication] terminate:self];
+        break;
+
+      default:
+        [super keyDown:theEvent];
+        break;
+    }
+  } else {
+    [super keyDown:theEvent];
+  }
+}
+
 @end
